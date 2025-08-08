d881a902e0a385541c63c1dacd5cc9bb057573b7
Elias <fernandezvillaelias41@gmail.com>
2025-08-08 18:15:55 -0300

    la api de noticias se consume correctamente, falta implementarla en el xaml de noticias para que se puedan mostrar sus datos ahi

--- obligatorioMauiEFyCS/Noticias.xaml.cs
+++ obligatorioMauiEFyCS/Noticias.xaml.cs
@@ -1,9 +1,35 @@
+using Newtonsoft.Json;
+
 namespace obligatorioMauiEFyCS;
 
 public partial class Noticias : ContentPage
 {
-	public Noticias()
-	{
-		InitializeComponent();
-	}
+    public Noticias()
+    {
+        InitializeComponent();
+
+        CargarNoticiasAsync();
+    }
+
+    public async void CargarNoticiasAsync()
+    {
+        string url = "https://newsdata.io/api/1/latest?apikey=pub_1edc0dcb4e2d42189bd67947c6937530&country=uy&language=es&image=1";
+
+        using (HttpClient client = new HttpClient())
+        {
+            var response = await client.GetAsync(url);
+            if (!response.IsSuccessStatusCode)
+            {
+                await DisplayAlert("Error", "No se pudo obtener el clima.", "OK");
+            }
+
+            else
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                var noticias = JsonConvert.DeserializeObject<NoticiasModel>(json);
+            }
+        }
+
+
+    }
 }
