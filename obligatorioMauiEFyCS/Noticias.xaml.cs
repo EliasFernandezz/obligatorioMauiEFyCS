cc305c829d735789e7621532b55d586c2a4be2cc
Elias <fernandezvillaelias41@gmail.com>
2025-08-12 01:43:52 -0300

    api de noticias mayormente terminada, queda poner las url hacia las noticias reales y ya quedaria listo

--- obligatorioMauiEFyCS/Noticias.xaml.cs
+++ obligatorioMauiEFyCS/Noticias.xaml.cs
@@ -27,9 +27,30 @@ public partial class Noticias : ContentPage
             {
                 var json = await response.Content.ReadAsStringAsync();
                 var noticias = JsonConvert.DeserializeObject<NoticiasModel>(json);
+
+                tituloNoticia1.Text = noticias.Results[0].Title;
+                tituloNoticia2.Text = noticias.Results[1].Title;
+                tituloNoticia3.Text = noticias.Results[2].Title;
+                tituloNoticia4.Text = noticias.Results[3].Title;
+                tituloNoticia5.Text = noticias.Results[4].Title;
+                tituloNoticia6.Text = noticias.Results[5].Title;
+
+                imagenNoticia1.Source = noticias.Results[0].ImageUrl;
+                imagenNoticia2.Source = noticias.Results[1].ImageUrl;
+                imagenNoticia3.Source = noticias.Results[2].ImageUrl;
+                imagenNoticia4.Source = noticias.Results[3].ImageUrl;
+                imagenNoticia5.Source = noticias.Results[4].ImageUrl;
+                imagenNoticia6.Source = noticias.Results[5].ImageUrl;
+
+                descripcionNoticia1.Text = noticias.Results[0].Description;
+                descripcionNoticia2.Text = noticias.Results[1].Description;
+                descripcionNoticia3.Text = noticias.Results[2].Description;
+                descripcionNoticia4.Text = noticias.Results[3].Description;
+                descripcionNoticia5.Text = noticias.Results[4].Description;
+                descripcionNoticia6.Text = noticias.Results[5].Description;
+
             }
         }
-
 
     }
 }
