522a2ad4125a00a10d918d1627972a436bbe8175
Elias <fernandezvillaelias41@gmail.com>
2025-08-13 01:27:32 -0300

    links para ir a las noticias reales hechos y se hizo que las noticias se muestren bien en celular y computadora

--- obligatorioMauiEFyCS/Noticias.xaml.cs
+++ obligatorioMauiEFyCS/Noticias.xaml.cs
@@ -1,4 +1,5 @@
 using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace obligatorioMauiEFyCS;
 
@@ -8,9 +9,69 @@ public partial class Noticias : ContentPage
     {
         InitializeComponent();
 
+        if (DeviceInfo.Idiom == DeviceIdiom.Phone)
+        {
+            gridNoticias.RowDefinitions.Clear();
+            gridNoticias.ColumnDefinitions.Clear();
+            for (int i = 0; i < 6; i++)
+            {
+                gridNoticias.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            }
+            gridNoticias.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            frameNoticia1.SetValue(Grid.RowProperty, 0);
+            frameNoticia1.SetValue(Grid.ColumnProperty, 0);
+
+            frameNoticia2.SetValue(Grid.RowProperty, 1);
+            frameNoticia2.SetValue(Grid.ColumnProperty, 0);
+
+            frameNoticia3.SetValue(Grid.RowProperty, 2);
+            frameNoticia3.SetValue(Grid.ColumnProperty, 9);
+
+            frameNoticia4.SetValue(Grid.RowProperty, 3);
+            frameNoticia4.SetValue(Grid.ColumnProperty, 0);
+
+            frameNoticia5.SetValue(Grid.RowProperty, 4);
+            frameNoticia5.SetValue(Grid.ColumnProperty, 9);
+
+            frameNoticia6.SetValue(Grid.RowProperty, 5);
+            frameNoticia6.SetValue(Grid.ColumnProperty, 0);
+        }
+        else
+        {
+            gridNoticias.RowDefinitions.Clear();
+            gridNoticias.ColumnDefinitions.Clear();
+            for (int i = 0; i < 2; i++)
+            {
+                gridNoticias.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                gridNoticias.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            }
+            frameNoticia1.SetValue(Grid.RowProperty, 0);
+            frameNoticia1.SetValue(Grid.ColumnProperty, 0);
+
+            frameNoticia2.SetValue(Grid.RowProperty, 0);
+            frameNoticia2.SetValue(Grid.ColumnProperty, 1);
+
+            frameNoticia3.SetValue(Grid.RowProperty, 0);
+            frameNoticia3.SetValue(Grid.ColumnProperty, 2);
+
+            frameNoticia4.SetValue(Grid.RowProperty, 1);
+            frameNoticia4.SetValue(Grid.ColumnProperty, 0);
+
+            frameNoticia5.SetValue(Grid.RowProperty, 1);
+            frameNoticia5.SetValue(Grid.ColumnProperty, 1);
+
+            frameNoticia6.SetValue(Grid.RowProperty, 1);
+            frameNoticia6.SetValue(Grid.ColumnProperty, 2);
+        }
+
         CargarNoticiasAsync();
     }
 
+    NoticiasModel? noticias;
     public async void CargarNoticiasAsync()
     {
         string url = "https://newsdata.io/api/1/latest?apikey=pub_1edc0dcb4e2d42189bd67947c6937530&country=uy&language=es&image=1";
@@ -26,7 +87,7 @@ public partial class Noticias : ContentPage
             else
             {
                 var json = await response.Content.ReadAsStringAsync();
-                var noticias = JsonConvert.DeserializeObject<NoticiasModel>(json);
+                noticias = JsonConvert.DeserializeObject<NoticiasModel>(json);
 
                 tituloNoticia1.Text = noticias.Results[0].Title;
                 tituloNoticia2.Text = noticias.Results[1].Title;
@@ -52,5 +113,35 @@ public partial class Noticias : ContentPage
             }
         }
 
+    }
+
+    private async void TapGestureRecognizer_Enlace1(object sender, TappedEventArgs e)
+    {
+        await Browser.Default.OpenAsync(noticias.Results[0].SourceUrl, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async void TapGestureRecognizer_Enlace2(object sender, TappedEventArgs e)
+    {
+        await Browser.Default.OpenAsync(noticias.Results[1].SourceUrl, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async void TapGestureRecognizer_Enlace3(object sender, TappedEventArgs e)
+    {
+        await Browser.Default.OpenAsync(noticias.Results[2].SourceUrl, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async void TapGestureRecognizer_Enlace4(object sender, TappedEventArgs e)
+    {
+        await Browser.Default.OpenAsync(noticias.Results[3].SourceUrl, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async void TapGestureRecognizer_Enlace5(object sender, TappedEventArgs e)
+    {
+        await Browser.Default.OpenAsync(noticias.Results[4].SourceUrl, BrowserLaunchMode.SystemPreferred);
+    }
+
+    private async void TapGestureRecognizer_Enlace6(object sender, TappedEventArgs e)
+    {
+        await Browser.Default.OpenAsync(noticias.Results[5].SourceUrl, BrowserLaunchMode.SystemPreferred);
     }
 }
